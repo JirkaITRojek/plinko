352924ea501c0e75a7dd7e9dacd5cd0d73b736ac
JirkaITRojek <98584715+JirkaITRojek@users.noreply.github.com>
2023-01-24 07:47:17 +0100

    ninth commit

--- js/scretch.js
+++ js/scretch.js
@@ -8,7 +8,6 @@ var world;
 var particles = [];
 var plinkos = [];
 var bounds = [];
-var paddle = [];
 var screenWidht = screen.width;
 var screenHeight = screen.height / 9 * 8;
 var cols = 20;
@@ -21,12 +20,15 @@ let d =0;
 
 function setup() {
   createCanvas(screenWidht, screenHeight);
+  //Vytvori canvas
   colorMode(HSB);
+  //Nastavy alternativu RGB
   engine = Engine.create();
   world = engine.world;
 
 
   newParticle();
+  //V teto casti se pocita pres for kam dat plinka nebo ty bile castice ktere se nehybou
   var spacing = width / cols;
   for (var j = 0; j < rows; j++) {
     for (var i = 0; i < cols + 1; i++) {
@@ -37,11 +39,13 @@ function setup() {
       var y = spacing + j * spacing;
       var p = new Plinko(x, y, 16);
       plinkos.push(p);
+      //Tady se to posle do plinko.js
     }
   }
 
   var b = new Boundary(width / 2, height + 50, width, 100);
   bounds.push(b);
+  //Tohle nastavy okraje pres ktere neprojdou kulicky, nastaveno pres boundary.js
 
   for (var i = 0; i < cols + 2; i++) {
     var x = i * spacing;
@@ -49,12 +53,14 @@ function setup() {
     var w = 10;
     var y = height - h / 2;
     var b = new Boundary(x, y, w, h);
+    //Tohle udela sloupce ktere vytvari bloky do kterych padaji kulicky zase delano pres boundary.js
     bounds.push(b);
 
   }
   
  }
 addEventListener("keypress", function(e){
+  //Posloucha zmacknuti tlacitekk a pres if prirazuje hodnoty promene
     if(e.code == 'KeyA'){
       a = 1;
     }
@@ -72,6 +78,7 @@ addEventListener("keypress", function(e){
   })
 
 function newParticle() {
+  //Vytvori nove kulicky pouze kdyz byly zmacknute spravne klavesy
   if(s ==1){
     var p = new Particle(screenWidht / 2, 0, 10);
   particles.push(p);
@@ -89,14 +96,17 @@ function newParticle() {
 
 function draw() {
   background(0, 0, 0);
-  //Zde si zmen rychlost padani
+  //Nastavi pozadi na cerno
   if (frameCount % (speed * 10) == 0) {
+    //Spusti funkci newParticle pouze kdyz frameCount deleno promennou speed kterou zadal uzivatel vynasobena deseti je delen bezezbytku
     newParticle();
   }
   Engine.update(engine, 1000 / 30);
   for (var i = 0; i < particles.length; i++) {
+    //Ukazuje kulicky s indexem [i]
     particles[i].show();
     if (particles[i].isOffScreen()) {
+      //odstranuje vsechny kulicky ktere jsou mimo obrazovku 
       World.remove(world, particles[i].body);
       particles.splice(i, 1);
       i--;
